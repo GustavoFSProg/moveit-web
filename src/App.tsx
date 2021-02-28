4b6a23761d063693fc9b7def33af1fe9780de3cb
Gustavo <gustavosohne38@gmail.com>
2021-02-27 21:49:31 -0300

    indo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import Button from './components/Button'
 import './styles/global.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <button style={{ height: '2rem' }}></button>
+    </div>
+  )
 }
 
 export default App
